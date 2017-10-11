7b6178843059d9ba875d55e38b7090549b1ab40b
jdr1 <jay.rathod@gmail.com>
2017-10-11 19:29:15 +0100

    Added my name and roll

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+ var formattedHeaderName = HTMLheaderName.replace("%data%", "Jayendra Rathod");
+ var formattedHeaderRole = HTMLheaderRole.replace("%data%", "Team Lead Tools Programmer");
+
+ $("#header").prepend(formattedHeaderRole);
+ $("#header").prepend(formattedHeaderName);
+ 
